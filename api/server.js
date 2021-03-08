808a8d8a1629eac47477b67866bd0a838b474f5e
SergiiPodlishchuk <podluy23@gmail.com>
2021-03-08 23:18:17 +0200

    work with lat lon

--- api/server.js
+++ api/server.js
@@ -3,19 +3,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const { graphqlHTTP } = require("express-graphql");
+const axios = require("axios");
 
-const tripsRouters = require("./trips/trips.routers");
+// const tripsRouters = require("./trips/trips.routers");
 
 const { ApolloServer, gql } = require("apollo-server-express");
-
-// const schema = require("./shema");
 
 const tripModel = require("./trips/trips.model");
 
 const PORT = 4545;
 const MONGO_DB_URL =
   "mongodb+srv://SergeiPodlishchuk:fbm6!E-HPvYMd2k@cluster0.x1opf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const MAPBOX_API_TOKEN =
+  "pk.eyJ1Ijoic2VyZ2lpcG9kbGlzaGNodWsiLCJhIjoiY2ttMGJjc2o0NDF6ODJubjFzdGV5ZTJtNiJ9.upVxFI-1HPQg9YIBuOY0tw";
+
+axios.defaults.baseURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
 (async function () {
   return await mongoose.connect(
@@ -65,6 +68,20 @@ const typeDefs = gql`
   }
 `;
 
+const takeLocation = async (lat, lon) => {
+  const getPlace = await axios.get(
+    `${lat},${lon}.json?access_token=${MAPBOX_API_TOKEN}`
+  );
+  //   console.log(getPlace);
+  const dataLocation = await getPlace.data.features[0].place_name;
+
+  if (dataLocation) {
+    return dataLocation;
+  } else {
+    return "i do not know this place";
+  }
+};
+
 const resolvers = {
   Query: {
     trips: async () => {
@@ -74,11 +91,16 @@ const resolvers = {
   Mutation: {
     createTrip: async (parent, { input }, context) => {
       const obj = JSON.parse(JSON.stringify(input));
-      console.log(obj);
+
+      const locationFrom = obj.fromPlaceId.split(",");
+      const locationTo = obj.toPlaceId.split(",");
+
+      const from = await takeLocation(locationFrom[0], locationFrom[1]);
+      const to = await takeLocation(locationTo[0], locationTo[1]);
 
       const newTrip = {
-        from: { name: "KH" },
-        to: { name: "KIEV" },
+        from: { name: from },
+        to: { name: to },
       };
 
       await tripModel.create(newTrip);
@@ -96,5 +118,5 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () => {
-  console.log(`Server start on port ${PORT} ${server.graphqlPath}`);
+  console.log(`Server start on port ${PORT}${server.graphqlPath}`);
 });
